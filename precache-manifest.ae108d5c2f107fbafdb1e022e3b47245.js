2f8d5ac7c55b725ed613eab73cf18c2e1a6b86f7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-29 20:41:27 +0000

    Deploy fabriciofmsilva/audio-app to github.com/fabriciofmsilva/audio-app.git:gh-pages

---
+++ precache-manifest.ae108d5c2f107fbafdb1e022e3b47245.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "icon_128x128.d10b9a8851a612a93519d28a0cb881f0.png"
+  },
+  {
+    "url": "icon_192x192.50b614b25e04baef7ffcd87c938a69fb.png"
+  },
+  {
+    "url": "icon_256x256.049911e843023a6b4cbdbaa90dfe5599.png"
+  },
+  {
+    "url": "icon_512x512.e383489f675e31907ff3736b80745b8d.png"
+  },
+  {
+    "revision": "3ac8937c7e0d49500416e8c5513ed788",
+    "url": "index.html"
+  },
+  {
+    "url": "main.c9ea97f0a89ac887c497.css"
+  },
+  {
+    "url": "main.c9ea97f0a89ac887c497.js"
+  },
+  {
+    "url": "manifest.63d592d62a59eb0dab355bb36b3c1bef.json"
+  }
+]);
